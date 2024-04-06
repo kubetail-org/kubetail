8f1228689682519f1735327f74197f761f4f28e2
Andres Morey <andresmarcel@gmail.com>
2024-04-06 15:59:13 +0300

    linter passing

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -43,6 +43,6 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/require-default-props': 'off',
     'react-hooks/exhaustive-deps': 'off',
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react-refresh/only-export-components': 'off', // re-examine this later
   },
 }
