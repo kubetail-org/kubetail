d697866910c7e8d4a6b7f9364e3f673ff3e91245
Andres Morey <andresmarcel@gmail.com>
2024-05-02 15:35:50 +0300

    set up toast ui to display graphql errors

--- frontend/src/pages/_root.tsx
+++ frontend/src/pages/_root.tsx
@@ -13,33 +13,84 @@
 // limitations under the License.
 
 import { XCircleIcon } from '@heroicons/react/24/outline';
-import { useEffect } from 'react';
-import { Toaster, resolveValue } from 'react-hot-toast';
+import { useEffect, useState } from 'react';
+import toastlib, { useToaster, resolveValue } from 'react-hot-toast';
+import type { Toast } from 'react-hot-toast';
 import { Outlet } from 'react-router-dom';
 
+import Button from '@kubetail/ui/elements/Button';
+
+import Modal from '@/components/elements/Modal';
 import { joinPaths, getBasename } from '@/lib/helpers';
 
-const CustomToaster = () => (
-  <Toaster position="bottom-center">
-    {(t) => (
-      <div className="rounded-md bg-red-50 p-4">
-        <div className="flex">
-          <div className="flex-shrink-0">
-            <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
-          </div>
-          <div className="ml-3">
-            <h3 className="text-sm font-medium text-red-800">
-              Error
-            </h3>
-            <div className="mt-2 text-sm text-red-700">
-              {resolveValue(t.message, t)}
-            </div>
-          </div>
-        </div>
-      </div>
-    )}
-  </Toaster>
+const QueryError = ({ toast }: { toast: Toast }) => (
+  <div className="relative bg-red-100 border-2 border-red-200 p-1">
+    <button
+      type="button"
+      className="absolute top-[-10px] left-[-10px] bg-red-100 rounded-full cursor-pointer"
+      onClick={() => toastlib.remove(toast.id)}
+      aria-label="remove"
+    >
+      <XCircleIcon
+        className="h-5 w-5 text-red-300 hover:text-red-400"
+        aria-hidden="true"
+      />
+    </button>
+    {resolveValue(toast.message, toast)}
+  </div>
 );
+
+const CustomToaster = () => {
+  const { toasts } = useToaster();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  if (!toasts.length) return null;
+
+  const handleRemoveAllClick = () => {
+    toastlib.remove();
+  };
+
+  return (
+    <div className="fixed bottom-[28px] right-[5px] p-2 bg-red-100 border-2 border-red-200">
+      <button
+        type="button"
+        className="absolute top-[-10px] left-[-10px] bg-red-100 rounded-full cursor-pointer"
+        onClick={handleRemoveAllClick}
+        aria-label="remove all"
+      >
+        <XCircleIcon
+          className="h-5 w-5 text-red-300 hover:text-red-400"
+          aria-hidden="true"
+        />
+      </button>
+      <button
+        type="button"
+        onClick={() => setModalIsOpen(true)}
+      >
+        Query Errors
+        {`(${toasts.length})`}
+      </button>
+      <Modal
+        className="max-w-[500px]"
+        open={modalIsOpen}
+        onClose={() => setModalIsOpen(false)}
+      >
+        <Modal.Title>
+          <div className="flex justify-between">
+            <span>
+              Query Errors
+              {`(${toasts.length})`}
+            </span>
+            <Button intent="danger" size="xs" onClick={() => toastlib.remove()}>Dismiss All</Button>
+          </div>
+        </Modal.Title>
+        <div className="text-sm space-y-4">
+          {toasts.map((toast) => <QueryError toast={toast} />)}
+        </div>
+      </Modal>
+    </div>
+  );
+};
 
 export default function Root() {
   // update favicon location
