aad23c315c978e70b4e977b42b681a2f0db76a26
Andres Morey <andresmarcel@gmail.com>
2024-06-06 17:40:09 +0300

    fixed https://github.com/kubetail-org/kubetail/issues/95

--- backend/graph/schema.resolvers.go
+++ backend/graph/schema.resolvers.go
@@ -1,17 +1,3 @@
-// Copyright 2024 Andres Morey
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package graph
 
 // This file will be automatically regenerated based on the schema, any resolver implementations
@@ -390,7 +376,6 @@ func (r *subscriptionResolver) CoreV1NamespacesWatch(ctx context.Context, option
 	}()
 
 	return outCh, nil
-
 }
 
 // CoreV1NodesWatch is the resolver for the coreV1NodesWatch field.
