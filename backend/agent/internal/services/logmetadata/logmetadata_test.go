2259b6a233eca7b5bd8c57ebe124d75dfc4e896b
Andres Morey <andresmarcel@gmail.com>
2024-07-28 11:57:55 -0400

    wip

--- backend/agent/internal/services/logmetadata/logmetadata_test.go
+++ backend/agent/internal/services/logmetadata/logmetadata_test.go
@@ -16,6 +16,7 @@ package logmetadata
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"os"
 	"path"
@@ -31,18 +32,23 @@ import (
 
 type LogMetadataTestSuite struct {
 	suite.Suite
-	grpcServer *grpc.Server
-	grpcConn   *grpc.ClientConn
-	logsDir    string
+	grpcServer       *grpc.Server
+	grpcConn         *grpc.ClientConn
+	podLogsDir       string
+	containerLogsDir string
 }
 
 func (suite *LogMetadataTestSuite) SetupSuite() {
-	// Create a temporary directory in the default location for temporary files.
-	logsDir, err := os.MkdirTemp("", "logmetadata")
+	// temporary directory for pod logs
+	podLogsDir, err := os.MkdirTemp("", "logmetadata-podlogsdir")
+	suite.Require().Nil(err)
+
+	// temporary directory for container log links
+	containerLogsDir, err := os.MkdirTemp("", "logmetadata-containerlogsdir")
 	suite.Require().Nil(err)
 
 	// init service
-	s, err := NewLogMetadataService("node-name", logsDir)
+	s, err := NewLogMetadataService("node-name", containerLogsDir)
 	suite.Require().Nil(err)
 
 	// init server
@@ -68,42 +74,81 @@ func (suite *LogMetadataTestSuite) SetupSuite() {
 	suite.Require().Nil(err)
 
 	// save references
-	suite.logsDir = logsDir
+	suite.podLogsDir = podLogsDir
+	suite.containerLogsDir = containerLogsDir
 	suite.grpcServer = grpcServer
 	suite.grpcConn = grpcConn
 }
 
 func (suite *LogMetadataTestSuite) TearDownSuite() {
-	defer os.RemoveAll(suite.logsDir)
+	defer os.RemoveAll(suite.containerLogsDir)
+	defer os.RemoveAll(suite.podLogsDir)
 	suite.grpcConn.Close()
 	suite.grpcServer.Stop()
 }
 
-func (suite *LogMetadataTestSuite) TestList() {
-	// create temp files
-	f1, err := os.Create(path.Join(suite.logsDir, "podname_default_containername-id123.log"))
+func (suite *LogMetadataTestSuite) createContainerLogFile(namespace string, podName string, containerName string, containerID string) *os.File {
+	// create pod log file
+	f, err := os.CreateTemp(suite.podLogsDir, "logmetadata-*.log")
 	suite.Require().Nil(err)
+
+	// add soft link to container logs dir
+	target := f.Name()
+	link := path.Join(suite.containerLogsDir, podName+"_"+namespace+"_"+containerName+"-"+containerID+".log")
+	fmt.Println(target)
+	fmt.Println(link)
+	err = os.Symlink(target, link)
+	suite.Require().Nil(err)
+
+	return f
+}
+
+func (suite *LogMetadataTestSuite) TestList() {
+	// create files
+	f1 := suite.createContainerLogFile("ns1", "pn1", "cn", "123")
+	defer f1.Close()
 	f1.Write([]byte("12345"))
+
+	f2 := suite.createContainerLogFile("ns2", "pn2", "cn", "abc")
+	defer f2.Close()
+	f2.Write([]byte("abc"))
 
 	// init client
 	client := agentpb.NewLogMetadataServiceClient(suite.grpcConn)
 
-	// make request
-	req := &agentpb.LogMetadataListRequest{
-		Namespaces: []string{"default"},
+	// make request 1
+	req1 := &agentpb.LogMetadataListRequest{
+		Namespaces: []string{"ns1"},
 	}
-	resp, err := client.List(context.Background(), req)
+	resp1, err := client.List(context.Background(), req1)
 	suite.Require().Nil(err)
-	suite.Equal(1, len(resp.Items))
+	suite.Equal(1, len(resp1.Items))
 
-	item := resp.Items[0]
-	suite.Equal("id123", item.Id)
-	suite.Equal("node-name", item.Spec.NodeName)
-	suite.Equal("default", item.Spec.Namespace)
-	suite.Equal("podname", item.Spec.PodName)
-	suite.Equal("containername", item.Spec.ContainerName)
-	suite.Equal("id123", item.Spec.ContainerId)
-	suite.Equal(int64(5), item.FileInfo.Size)
+	item1 := resp1.Items[0]
+	suite.Equal("123", item1.Id)
+	suite.Equal("node-name", item1.Spec.NodeName)
+	suite.Equal("ns1", item1.Spec.Namespace)
+	suite.Equal("pn1", item1.Spec.PodName)
+	suite.Equal("cn", item1.Spec.ContainerName)
+	suite.Equal("123", item1.Spec.ContainerId)
+	suite.Equal(int64(5), item1.FileInfo.Size)
+
+	// make request 2
+	req2 := &agentpb.LogMetadataListRequest{
+		Namespaces: []string{"ns2"},
+	}
+	resp2, err := client.List(context.Background(), req2)
+	suite.Require().Nil(err)
+	suite.Equal(1, len(resp1.Items))
+
+	item2 := resp2.Items[0]
+	suite.Equal("abc", item2.Id)
+	suite.Equal("node-name", item2.Spec.NodeName)
+	suite.Equal("ns2", item2.Spec.Namespace)
+	suite.Equal("pn2", item2.Spec.PodName)
+	suite.Equal("cn", item2.Spec.ContainerName)
+	suite.Equal("abc", item2.Spec.ContainerId)
+	suite.Equal(int64(3), item2.FileInfo.Size)
 }
 
 func (suite *LogMetadataTestSuite) TestWatch() {
