5362a5bef650b9cc91e75893e694cdb0fc5f27d2
Andres Morey <andresmarcel@gmail.com>
2024-11-24 07:48:57 +0300

    used apollo suspense query to implement loading modal

--- dashboard-ui/src/pages/_root.tsx
+++ dashboard-ui/src/pages/_root.tsx
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { useQuery } from '@apollo/client';
+import { useSuspenseQuery } from '@apollo/client';
 import { XCircleIcon } from '@heroicons/react/24/outline';
 import { useEffect, useState } from 'react';
 import toastlib, { useToaster, resolveValue } from 'react-hot-toast';
@@ -20,7 +20,6 @@ import type { Toast } from 'react-hot-toast';
 import { Outlet } from 'react-router-dom';
 
 import Button from '@kubetail/ui/elements/Button';
-import Spinner from '@kubetail/ui/elements/Spinner';
 
 import Modal from '@/components/elements/Modal';
 import * as ops from '@/lib/graphql/ops';
@@ -95,22 +94,6 @@ const CustomToaster = () => {
   );
 };
 
-const LoadingModal = () => (
-  <div className="relative z-10" role="dialog">
-    <div className="fixed inset-0 bg-chrome-500 bg-opacity-75" />
-    <div className="fixed inset-0 z-10 w-screen">
-      <div className="flex min-h-full items-center justify-center p-0 text-center">
-        <div className="relative transform overflow-hidden rounded-lg bg-background my-8 p-6 text-left shadow-xl">
-          <div className="flex items-center space-x-2">
-            <div>Connecting to cluster...</div>
-            <Spinner size="sm" />
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-);
-
 export default function Root() {
   // update favicon location
   useEffect(() => {
@@ -119,12 +102,10 @@ export default function Root() {
     el.setAttribute('href', joinPaths(getBasename(), '/favicon.ico'));
   }, []);
 
-  /*
-  const { loading } = useQuery(ops.READY_WAIT, {
+  useSuspenseQuery(ops.READY_WAIT, {
     fetchPolicy: 'no-cache',
     onError: console.log,
   });
-  */
 
   return (
     <>
