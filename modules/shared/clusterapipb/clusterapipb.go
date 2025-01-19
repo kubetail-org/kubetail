6679c0ae24d46fdff02ba27552d2ae580e9e4b28
Andres Morey <andresmarcel@gmail.com>
2025-01-19 08:57:01 +0300

    removed unused cluster_api.proto file

--- modules/shared/clusterapipb/clusterapipb.go
+++
@@ -1,3 +0,0 @@
-package clusterapipb
-
-//go:generate protoc --proto_path=../../../proto --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ../../../proto/cluster_api.proto
