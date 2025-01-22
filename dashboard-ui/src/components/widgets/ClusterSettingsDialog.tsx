6e48315621c9911c75f4273beb065bed04a02ad1
Andres Morey <andresmarcel@gmail.com>
2025-01-22 17:02:54 +0300

    wip

---
+++ dashboard-ui/src/components/widgets/ClusterSettingsDialog.tsx
@@ -0,0 +1,107 @@
+// Copyright 2024-2025 Andres Morey
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { useSubscription } from '@apollo/client';
+import { useEffect, useState } from 'react';
+
+import Form from '@kubetail/ui/elements/Form';
+
+import appConfig from '@/app-config';
+import Modal from '@/components/elements/Modal';
+import * as dashboardOps from '@/lib/graphql/dashboard/ops';
+
+const defaultKubeContext = appConfig.environment === 'cluster' ? '' : undefined;
+
+const KubeContextPicker = ({
+  className,
+  value,
+  setValue,
+}: {
+  className?: string;
+  value?: string;
+  setValue: (value: string) => void;
+}) => {
+  const { loading, data } = useSubscription(dashboardOps.KUBE_CONFIG_WATCH);
+  const kubeConfig = data?.kubeConfigWatch?.object;
+
+  // Set default value
+  useEffect(() => {
+    const defaultValue = kubeConfig?.currentContext;
+    if (defaultValue) setValue(defaultValue);
+  }, [loading]);
+
+  return (
+    <Form.Select
+      className={className}
+      value={value}
+      onChange={(ev) => setValue(ev.target.value)}
+      disabled={loading}
+    >
+      {loading ? (
+        <Form.Option>Loading...</Form.Option>
+      ) : (
+        kubeConfig && kubeConfig.contexts.map((context) => (
+          <Form.Option key={context.name} value={context.name}>{context.name}</Form.Option>
+        ))
+      )}
+    </Form.Select>
+  );
+};
+
+type ClusterSettingsDialogProps = {
+  isOpen?: boolean;
+  onClose: (value?: boolean) => void;
+};
+
+export const ClusterSettingsDialog = ({ isOpen = false, onClose }: ClusterSettingsDialogProps) => {
+  const [kubeContext, setKubeContext] = useState(defaultKubeContext);
+
+  return (
+    <Modal open={isOpen} onClose={onClose} className="!max-w-[550px]">
+      <Modal.Title>Cluster Settings</Modal.Title>
+      <div>
+        {appConfig.environment === 'desktop' && (
+          <KubeContextPicker
+            className="w-auto"
+            value={kubeContext}
+            setValue={setKubeContext}
+          />
+        )}
+        <Form className="mt-5">
+          <Form.Group>
+            <Form.Label>
+              Kubernetes API Token
+            </Form.Label>
+            <Form.Control placeholder="Token..." />
+          </Form.Group>
+          <Form.Fieldset>
+            <Form.Label>
+              Kubetail Cluster API
+            </Form.Label>
+            <div className="flex">
+              <Form.Group>
+                <Form.Label>Namespace</Form.Label>
+                <Form.Control placeholder="Namespace" />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Service Name</Form.Label>
+                <Form.Control placeholder="Service Name" />
+              </Form.Group>
+            </div>
+          </Form.Fieldset>
+        </Form>
+      </div>
+    </Modal>
+  );
+};
--- dashboard-ui/src/components/widgets/SettingsDropdown.tsx
+++ dashboard-ui/src/components/widgets/SettingsDropdown.tsx
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { CheckIcon } from '@radix-ui/react-icons';
 import { Settings as SettingsIcon } from 'lucide-react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '@kubetail/ui/elements/Button';
@@ -23,7 +23,6 @@ import {
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuPortal,
   DropdownMenuSeparator,
   DropdownMenuSub,
@@ -34,53 +33,56 @@ import {
 
 import appConfig from '@/app-config';
 import { useTheme, UserPreference } from '@/lib/theme';
+import { ClusterSettingsDialog } from '@/components/widgets/ClusterSettingsDialog';
 
 const SettingsDropdown = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   const { userPreference, setUserPreference } = useTheme();
 
   const themes = [UserPreference.Light, UserPreference.Dark, UserPreference.System];
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button intent="outline" size="sm"><SettingsIcon size={18} strokeWidth={1.5} /></Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuLabel>Settings</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem>
-            Cluster API
-          </DropdownMenuItem>
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger>Theme</DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent>
-                {themes.map((theme) => (
-                  <DropdownMenuCheckboxItem
-                    key={theme}
-                    checked={theme === userPreference}
-                    onCheckedChange={() => setUserPreference(theme)}
-                  >
-                    {theme}
-                  </DropdownMenuCheckboxItem>
-                ))}
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
-        </DropdownMenuGroup>
-        {appConfig.authMode === 'token' && (
-          <>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <Link to={`/auth/logout?${new URLSearchParams({ callbackUrl: window.location.pathname + window.location.search })}`}>
-                Sign out
-              </Link>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button intent="outline" size="sm"><SettingsIcon size={18} strokeWidth={1.5} /></Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-[150px]">
+          <DropdownMenuGroup>
+            <DropdownMenuItem onSelect={() => setIsDialogOpen(true)}>
+              Cluster Settings
             </DropdownMenuItem>
-          </>
-        )}
-      </DropdownMenuContent>
-    </DropdownMenu>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>Theme</DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  {themes.map((theme) => (
+                    <DropdownMenuCheckboxItem
+                      key={theme}
+                      checked={theme === userPreference}
+                      onCheckedChange={() => setUserPreference(theme)}
+                    >
+                      {theme}
+                    </DropdownMenuCheckboxItem>
+                  ))}
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+          </DropdownMenuGroup>
+          {appConfig.authMode === 'token' && (
+            <>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link to={`/auth/logout?${new URLSearchParams({ callbackUrl: window.location.pathname + window.location.search })}`}>
+                  Sign out
+                </Link>
+              </DropdownMenuItem>
+            </>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <ClusterSettingsDialog isOpen={isDialogOpen} onClose={() => setIsDialogOpen(false)} />
+    </>
   );
 };
 
