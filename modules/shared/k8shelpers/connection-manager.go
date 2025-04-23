07a78508f256eb60b08fea64f89c1bf3e198223d
Guillaume LEGRAIN <guillaume.legrain@numberly.com>
2025-04-23 10:03:11 +0200

    feat: add oidc plugin for k8s client

--- modules/shared/k8shelpers/connection-manager.go
+++ modules/shared/k8shelpers/connection-manager.go
@@ -24,6 +24,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/clientcmd/api"
