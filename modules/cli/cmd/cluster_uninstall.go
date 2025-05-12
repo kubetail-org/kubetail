a5ef3022691ae452688d3b73118345c7e0c454d6
rxinui <rainui.ly@gmail.com>
2025-05-12 17:05:28 +0200

    new: implement kubeconfig flag as global to CLI (#279)
    ## Summary
    
    Pre-work: add `kubeconfig` flag as persistent/global to all kubetail CLI
    commands.
    
    ## Help wanted
    
    shared helm.go is using `helm` cli which makes use of
    `*cli.EnvSettings`. This settings is used to define `kubeContext` from
    `--kube-context` flag. Is that the way `helm` makes use of the context?
    If so, I believe this _must be_ the same logic to give helm a
    kubeconfig.
    
    Could you please double check @amorey
    
    ---------
    
    Signed-off-by: rxinui <rainui.ly@gmail.com>

--- modules/cli/cmd/cluster_uninstall.go
+++ modules/cli/cmd/cluster_uninstall.go
@@ -35,15 +35,15 @@ var clusterUninstallCmd = &cobra.Command{
 	Long:  clusterUninstallHelp,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Get flags
-		kubeContext, _ := cmd.Flags().GetString("kube-context")
+		Kubeconfig, _ := cmd.Flags().GetString(KubeconfigFlag)
+		kubeContext, _ := cmd.Flags().GetString(KubeContextFlag)
 		//name, _ := cmd.Flags().GetString("name")
 		//namespace, _ := cmd.Flags().GetString("namespace")
 		name := helm.DefaultReleaseName
 		namespace := helm.DefaultNamespace
 
 		// Init client
-		client, err := helm.NewClient(kubeContext)
-		cli.ExitOnError(err)
+		client := helm.NewClient(helm.WithKubeContext(kubeContext), helm.WithKubeconfig(Kubeconfig))
 
 		// Uninstall
 		response, err := client.UninstallRelease(namespace, name)
@@ -61,7 +61,7 @@ func init() {
 	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	flagset := clusterUninstallCmd.Flags()
 	flagset.SortFlags = false
-	flagset.String("kube-context", "", "Name of the kubeconfig context to use")
+	flagset.String(KubeContextFlag, "", "Name of the kubeconfig context to use")
 	//flagset.String("name", helm.DefaultReleaseName, "Release name")
 	//flagset.StringP("namespace", "n", helm.DefaultNamespace, "Namespace to install into")
 }
