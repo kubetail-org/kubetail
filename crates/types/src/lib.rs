bdea3d4f81c83980cc0e7d1b0916b60ac6fddbfd
Christopher Valerio <christopher@valerio.guru>
2025-06-20 16:32:48 -0600

    feat: grpc server running, with health check and reflection
    Signed-off-by: Christopher Valerio <christopher@valerio.guru>

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -1,3 +1,6 @@
 pub mod cluster_agent {
     tonic::include_proto!("cluster_agent");
+
+    pub const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("topology_descriptor");
 }
