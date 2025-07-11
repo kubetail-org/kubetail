69d285b2856440dc327075e93ab107e220533994
Andres Morey <andresmarcel@gmail.com>
2025-07-11 00:28:39 -0400

    chore: upgrade dashboard-ui dependencies (#539)
    Signed-off-by: Andres Morey <andresmarcel@gmail.com>

--- dashboard-ui/vite.config.ts
+++ dashboard-ui/vite.config.ts
@@ -1,17 +1,17 @@
-/// <reference types="vitest/config" />
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import Unfonts from 'unplugin-fonts/vite';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv, mergeConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
 
 export default ({ mode }: { mode: string }) => {
   const env = loadEnv(mode, process.cwd());
 
   const backendTarget = `http://localhost:${env.VITE_DASHBOARD_PROXY_PORT}`;
 
-  return defineConfig({
+  const viteConfig = defineConfig({
     plugins: [
       tsconfigPaths(),
       svgr(),
@@ -61,10 +61,15 @@ export default ({ mode }: { mode: string }) => {
         },
       },
     },
+  });
+
+  const vitestConfig = defineVitestConfig({
     test: {
       environment: 'jsdom',
       globals: true,
       setupFiles: ['./vitest.setup.ts'],
     },
   });
+
+  return mergeConfig(viteConfig, vitestConfig);
 };
