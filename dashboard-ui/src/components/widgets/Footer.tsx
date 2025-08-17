5521be5e65cf7c914855f13a56be8bc7f10c077a
Andres Morey <andresmarcel@gmail.com>
2025-08-17 14:45:57 +0300

    new: mvp of home page with kubetail-ui-v2
    Signed-off-by: Andres Morey <andresmarcel@gmail.com>

--- dashboard-ui/src/components/widgets/Footer.tsx
+++ dashboard-ui/src/components/widgets/Footer.tsx
@@ -12,7 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Form from '@kubetail/ui/elements/Form';
+import { ChevronDownIcon } from 'lucide-react';
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@kubetail/ui/elements/select';
 
 import ServerStatus from '@/components/widgets/ServerStatus';
 import { useTheme, UserPreference } from '@/lib/theme';
@@ -21,21 +31,28 @@ import EnvironmentControl from './EnvironmentControl';
 export default function Footer() {
   const { userPreference, setUserPreference } = useTheme();
 
-  const handleChange = (ev: React.ChangeEvent<HTMLSelectElement>) => {
-    setUserPreference(ev.target.value as UserPreference);
+  const handleChange = (value: UserPreference) => {
+    setUserPreference(value);
   };
 
   return (
     <div className="h-[22px] bg-chrome-100 border-t border-chrome-divider text-sm flex justify-between items-center pl-[10px]">
-      <Form.Select
-        className="w-[65px] mt-0 py-0 pl-0 pr-0 h-auto border-0 focus:ring-offset-0 focus:ring-0 focus:border-transparent focus:ring-transparent text-xs bg-transparent"
-        value={userPreference}
-        onChange={handleChange}
-      >
-        <Form.Option value={UserPreference.System}>system</Form.Option>
-        <Form.Option value={UserPreference.Dark}>dark</Form.Option>
-        <Form.Option value={UserPreference.Light}>light</Form.Option>
-      </Form.Select>
+      <Select value={userPreference} onValueChange={handleChange}>
+        <SelectTrigger asChild>
+          <div className="cursor-pointer flex items-center gap-1">
+            <SelectValue />
+            <ChevronDownIcon className="size-4" />
+          </div>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Theme</SelectLabel>
+            <SelectItem value={UserPreference.System}>system</SelectItem>
+            <SelectItem value={UserPreference.Dark}>dark</SelectItem>
+            <SelectItem value={UserPreference.Light}>light</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
       <div className="flex">
         {import.meta.env.MODE === 'development' && <EnvironmentControl />}
         <ServerStatus />
