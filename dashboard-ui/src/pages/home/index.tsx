dceb32ccb0bb343cec766308b3855cd4e92eba03
Andres Morey <andres@kubetail.com>
2026-01-11 08:55:50 +0300

    chore: update copyright name and date (#887)
    Signed-off-by: Andres Morey <andres@kubetail.com>

--- dashboard-ui/src/pages/home/index.tsx
+++ dashboard-ui/src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-// Copyright 2024-2025 Andres Morey
+// Copyright 2024 The Kubetail Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
