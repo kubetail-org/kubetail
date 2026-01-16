c1b8a44c11975f698b47e6e256ffabf95ea12fb9
Andres Morey <andres@kubetail.com>
2026-01-16 17:06:59 +0300

    ref: fix config.json syntax, make config.toml comments more idomatic, add all files to go:generate
    Signed-off-by: Andres Morey <andres@kubetail.com>

--- modules/cli/assets/embed.go
+++ modules/cli/assets/embed.go
@@ -3,6 +3,8 @@ package assets
 import "embed"
 
 //go:generate cp ../../../config/default/cli.yaml ./config.yaml
+//go:generate cp ../../../config/default/cli.toml ./config.toml
+//go:generate cp ../../../config/default/cli.json ./config.json
 
 //go:embed *
 var FS embed.FS
